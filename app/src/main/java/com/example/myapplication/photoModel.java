baa16d69e17c923636221969c178015c5119eef2
noyongamer88322 <noyongamer88322@gmail.com>
2024-09-04 00:23:18 +0600

    1

---
+++ app/src/main/java/com/example/myapplication/photoModel.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class photoModel {
+
+    String image;
+
+    public photoModel() {
+    }
+
+    public photoModel(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
